29c079e265a9d212ea6b640aa7e2c2776643753b
Guilherme Lopes <guilopes461@gmail.com>
2025-09-03 21:05:51 -0300

    did some of the main and got adding and seeing the table working

--- backend/app.py
+++ backend/app.py
@@ -3,13 +3,45 @@ from datetime import date
 from enum import Enum, auto
 
 def main():
-    print('\n')
-    
+    choice = 0
+    while choice != 4:
+        print('\n1 -> Adicionar Hábito\n2 -> Remover Hábito\n3 -> Verificar Hábitos\n4 -> Sair')
+        choice = int(input('Escolha qual ação você deseja realizar:\n'))
+        if choice == 1:
+            habit_type = str(input('Are you trying to build a good habit, or break a bad one? (Build/Break)\n'))
+            if habit_type == 'Build':
+                habit = str(input('What habit are you building?\n'))
+            if habit_type == 'Break':
+                habit = str(input('What habit are you trying to break?\n'))
+            goal = int(input("What's your goal?\n"))
+            unit = str(input("What's your habits unit of measure?\n"))
+            frequency = str(input("What's the frequency of your habit? (Daily/Weekly)\n"))
+            add_habit(habit, goal, habit_type, unit, frequency)
+        if choice == 2:
+            habit = str()
+        if choice == 3:
+            get_habits()
+        
 
+class HabitType(Enum):
+    BUILD = 'Build'
+    BREAK = 'Break'
 
 class Frequency(Enum):
     DAILY = 'Daily'
     WEEKLY = 'Weekly'
+
+def format_goals(s: str) -> list[str]:
+    '''
+    Receives a goal string *s* and separate the number, the unit, and the frequency
+    Examples
+    >>> format_goal(50 pages/week)
+    ['50', 'pages', 'week']
+    '''
+    goal = ''
+    unit = ''
+    frequency = ''
+     
 
 def get_connectiondb():
     connection = sqlite3.connect('habit.db')
@@ -18,28 +50,35 @@ def get_connectiondb():
 
 def get_habits():
     connection = get_connectiondb()
-    habits_cursor = connection.execute('SELECT id, name, target, unit, frequency FROM Habits')
+    habits_cursor = connection.execute('SELECT id, habit, goal, habit_type, unit, frequency FROM Habits')
     habits = [dict(row) for row in habits_cursor]
-    print('\nSeus habitos:\n---------------')
+    if not habits:
+        print("\nYou still don't have any habits:\n---------------")
+    print('\nYour habits:\n---------------')
     for h in habits:
-        hid, name, target, unit, frequency = h
+        hid = h['id']
+        habit = h['habit']
+        habit_type = h['habit_type']
+        goal = h['goal']
+        unit = h['unit']
+        frequency = h['frequency']
         if frequency == Frequency.DAILY.value: 
             habits_cursor.execute("SELECT SUM(amount) FROM Logs WHERE habit_id=? AND date=?",
                                   (hid, str(date.today())),
                                   )
             total = habits_cursor.fetchone()[0] or 0
-            print(f"{hid}. {name} – Today: {total}/{target} {unit}")
+            print(f"{hid}. {habit} – Today: {total}/{goal} {unit}")
         elif frequency == Frequency.WEEKLY.value:
             habits_cursor.execute("SELECT SUM(amount) FROM Logs WHERE habit_id=? AND date>=date('now','-7 day')",
                                   (hid,),
                                   )
             total = habits_cursor.fetchone()[0] or 0
-            print(f"{hid}. {name} – This week: {total}/{target} {unit}")
+            print(f"{hid}. {habit} – This week: {total}/{goal} {unit}")
 
-def add_habit(name: str, target: int, unit: str, frequency: Frequency):
+def add_habit(name: str, goal: int, habit_type: int,unit: str, frequency: Frequency):
     connection = get_connectiondb()
-    connection.execute('INSERT INTO Habits (name, target, unit, frequency) VALUES (?, ?, ?, ?)', 
-                       (name, target, unit, frequency))
+    connection.execute('INSERT INTO Habits (habit, goal, habit_type, unit,frequency) VALUES (?, ?, ?, ?, ?)', 
+                       (name, goal, habit_type, unit, frequency))
     connection.commit()
     connection.close()
 
@@ -48,3 +87,6 @@ def delete_habit(id: int):
     connection.execute('DELETE FROM Habits WHERE id = ?', (id,))
     connection.commit()
     connection.close()
+
+if __name__ == '__main__':
+    main()
